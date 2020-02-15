22f936633ac04b625c56f386e212546224dc9d10
lzh <52725224+Yourdaylight@users.noreply.github.com>
2020-02-15 17:38:24 +0800

    Update spider.py

--- dangdang/dangdang/spiders/spider.py
+++ dangdang/dangdang/spiders/spider.py
@@ -15,8 +15,8 @@ class SpiderSpider(scrapy.Spider):
         for url,category in zip(urls,categories):#爬取分类链接的详情页
             text = re.findall('title="(.*?)" alt',category)#正则匹配，提取分类关键字信息
             for i in range(1,20):#爬取分页信息
-                url=url+'&page_index={}#pos'.format(i)
-                yield scrapy.Request(url=url,callback=lambda response, category=text : self.parse_subpage(response,category), dont_filter=True)
+                url_now=url+'&page_index="+str(i)
+                yield scrapy.Request(url=url_now,callback=lambda response, category=text : self.parse_subpage(response,category), dont_filter=True)
 
     def parse_subpage(self,response,category):
 
